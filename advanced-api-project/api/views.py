6a9978d5d5d7400dd4d2422b86fa071704e18577
Dxcraig <steviedoe20@gmail.com>
2025-12-07 20:39:01 +0000

    feat(views): implement CRUD views for Book model

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class BookListView(generics.ListAPIView):
+    """
+    View to retrieve all books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailView(generics.RetrieveAPIView):
+    """
+    View to retrieve a single book by ID.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookCreateView(generics.CreateAPIView):
+    """
+    View to add a new book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    """
+    View to modify an existing book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    """
+    View to remove a book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
