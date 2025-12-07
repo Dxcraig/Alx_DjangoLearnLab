fb71071818db2324a0d7415c542e2d0fc9356e08
Dxcraig <steviedoe20@gmail.com>
2025-12-07 22:23:06 +0000

    Add initial URL configuration for django_blog project

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Blog Post model.
+    
+    Represents a blog post with title, content, publication date, and author.
+    Each post is associated with a user (author) who can have multiple posts.
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    
+    class Meta:
+        ordering = ['-published_date']
+    
+    def __str__(self):
+        return self.title
