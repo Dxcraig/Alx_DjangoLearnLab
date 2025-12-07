351bd56e340384b22b123d749e0019e92aa57302
Dxcraig <steviedoe20@gmail.com>
2025-12-07 22:18:14 +0000

    feat(blog): initialize blog app with essential files including models, views, and admin registration fix(settings): update SECRET_KEY for enhanced security

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wy*%$z!r%_yotzbkeem1*#oq*9axa8i#0%3g@nwthmcx=x86g1'
+SECRET_KEY = 'django-insecure-x2mn)8#6h^^bh)+dhwofv7(zpfscuf!mcc+hgdjjy!g3=61pt8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
